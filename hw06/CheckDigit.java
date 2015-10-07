c8112788b62ebd6bcd0f0f437190ce2533f1773c
yiz319 <yiz319@lehigh.edu>
2015-10-07 02:58:34 +0000

    updated CheckDigit

---
+++ hw06/CheckDigit.java
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////////
+//Yiheng Zhu
+//hw06
+//CheckDigit Java Program
+//
+//  first compile th program 
+//      javac CheckDigit.java
+//  run the program
+//      java CheckDigit//
+
+import java.util.Scanner;
+//  define a class
+public class CheckDigit{
+    
+// add main method
+  public static void main(String[] args){
+      System.out.println("I cant finish it because it is too hard!");
+      System.out.println("i will hand it in later!");
+  }
+}
